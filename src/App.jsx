9f8ce383eb3b15876114488343677ea9117e4397
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-03-07 21:12:02 +0300

    the code for the request to the server is written + the received data is transferred to the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import './App.scss'
+import React, { useCallback, useEffect, useState } from 'react';
 import { SearchInput } from './components/SearchInput/SearchInput';
+import { Card } from './components/Card/Card';
+import urlData from './data/data';
+import './App.scss'
 
 
 function App() {
+  const [dataUsers, setDataUsers] = useState({})
+  const [userValue, setUserValue] = useState('')
+  const [hideСard, setHideCard] = useState(true)
+
+  const getUsersData = useCallback(async () => {
+    try {
+      const response = await fetch(urlData + userValue)
+      const userData = await response.json()
+      setDataUsers(userData)
+      setHideCard(false)
+    } catch (error) {
+      console.error()
+    }
+  }, [userValue])
+
+  const changeInputHandler = (e) => {
+    setUserValue(e.target.value)
+  }
+
   return (
     <div className='App'>
-      <SearchInput/>
+      <SearchInput onChange={changeInputHandler} onClick={getUsersData} />
+      {
+        hideСard ? '' : <Card data={dataUsers} />
+      }
     </div>
   );
 }
