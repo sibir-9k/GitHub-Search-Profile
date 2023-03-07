30ec8a7e25511a165e5f600c0faf2b9c292e51ca
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-03-07 21:08:58 +0300

    icon added and display styles fixed

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { BsGithub } from 'react-icons/bs'
 import './SearchInput.scss'
 
 
-export const SearchInput = () => {
+export const SearchInput = ({ onChange, onClick }) => {
   return (
-    <>
+    <div className='search-input'>
       <h1>GitHub Search Profile</h1>
-      <form action="">
-        <input type="text" placeholder='find github profile...'/>
-      </form>
-    </>
-
+      <div className='search-input__form'>
+        <input type="text" placeholder='enter your github profile nickname' onChange={onChange} />
+        <button onClick={onClick}>Search <BsGithub /></button>
+      </div>
+    </div>
   )
 }
